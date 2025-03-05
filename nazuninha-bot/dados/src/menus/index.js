8a53b1eccd32b677bcbd35538ab31add0a1497d2
Hiudy <153995129+hiudyy@users.noreply.github.com>
2025-03-05 08:46:55 -0300

    Add nazuninha-bot/dados/src/menus/index.js 1741175213898

---
+++ nazuninha-bot/dados/src/menus/index.js
@@ -0,0 +1,11 @@
+const menu = require(__dirname+'/menu.js');
+const menudown = require(__dirname+'/menudown.js');
+const menuadm = require(__dirname+'/menuadm.js');
+const menubn = require(__dirname+'/menubn.js');
+const menuDono = require(__dirname+'/menudono.js');
+const menuMembros = require(__dirname+'/menumemb.js');
+const menuFerramentas = require(__dirname+'/ferramentas.js');
+const menuSticker = require(__dirname+'/menufig.js');
+const menuIa = require(__dirname+'/menuia.js');
+
+module.exports = { menuadm, menudown, menu, menubn, menuDono, menuMembros, menuFerramentas, menuSticker, menuIa };
